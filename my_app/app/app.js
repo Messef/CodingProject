bedc0141832db7587100a14a35438a548a60bd19
Messef <Akkubear@gmail.com>
2023-09-03 12:18:19 -0400

    Added an image to the website
    Changes on page.tsx

--- my_app/app/app.js
+++ my_app/app/app.js
@@ -1,12 +1,12 @@
 'use client'
+import ReactDOM from 'react-dom/client';
 import { useRef, useState } from 'react';
-import Image from 'next/image';
-import house from './images/house.jpg'
+
 export default function App() {
   const windowSize = useRef([window.innerWidth, window.innerHeight]);
   
   let [x, setx] = useState(0);
-  function func (x: any ) {
+  function func (x ) {
     setx(x+=1)
     console.log(x)
   }
@@ -16,20 +16,18 @@ export default function App() {
     padding: "10px",
     fontFamily: "Arial"
   };
-  function otherfunc(x: number) {
+  function yes(x) {
     setx(x=0)
 
   }
   return (
     <>
+    
     <div style={{height: windowSize.current[1], width: windowSize.current[0], background: 'white'}}>
       
       <button id = 'button' onClick={() => func(x)} style={mystyle}> Press me!</button>
       <p id='thisthing'>{x}</p>
-      <button id = 'buttontwo' onClick={() => otherfunc(x)}> Press me to clear!</button>
-      <div>
-        <Image src={house} alt="Picture of a house" onClick = {() => otherfunc(x)}/>
-      </div>
+      <button id = 'buttontwo' onClick={() => yes(x)}> Press me to clear!</button>
     </div>
     </>
   );
