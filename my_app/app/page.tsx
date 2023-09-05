1d57171e724b1a0492eb330d50131e200e909d15
Messef <Akkubear@gmail.com>
2023-09-05 14:56:17 -0400

    click speed test made using timers

--- my_app/app/page.tsx
+++ my_app/app/page.tsx
@@ -1,27 +1,44 @@
 'use client'
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import Image from 'next/image';
 import house from './images/house.jpg'
+
 import "./both.js"
 export default function App() {
+  
   const windowSize = useRef([window.innerWidth, window.innerHeight]);
   // Backend JS
-  
+  let [msg, setmsg] = useState("Click me to start")
+  let [othermsg, othersetmsg] = useState("")
   let [x, setx] = useState(0);
-  let [bin, setbin] = useState(1);
-  function func (x: number ) {
-    setx(x+=1)
-    setbin(1)
-  }
-  function otherfunc(x: number) {
-    if (x!=0 && bin == 1) 
-    {
-      setx(x=0)
-    } else if (bin==0) { 
-      setx(x+=1)
+  const countRef = useRef(x);
+  countRef.current = x;
+  const [count, setCount] = useState(0);
+  function func (x: any ) {
+    
+    if (msg=="Click me to start") {
+      setmsg(msg="Press me to increase the number on screen!")
     }
-    console.log(bin)
-    setbin(0)
+    if (x==0) {
+      
+  useEffect(() => {
+    const id = setInterval(() => setCount((oldCount) => oldCount + 1), 1000);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
+      setTimeout(() => {
+        othersetmsg(othermsg="you got " + countRef.current/5 + " cps!")
+        alert("You're Done!")
+      
+      
+      }, 5000);
+    }
+    setx(x+=1)
+  }
+  function clear(x: number) {
+      setx(x=0)
   }
 
 
@@ -30,20 +47,23 @@ export default function App() {
     color: "white",
     backgroundColor: "DodgerBlue",
     padding: "10px",
-    fontFamily: "Arial",
+    fontFamily: "Georgia",
     border: 0
   };
   return (
     <>
     <div style={{height: windowSize.current[1], width: windowSize.current[0], background: 'white'}}>
       
-      <button id = 'button' onClick={() => func(x)} style={mystyle}> Press me!</button>
+      <button id = 'button' onClick={() => func(x)} style={mystyle}> {msg}</button>
+      <p></p>
       <p id='thisthing'>{x}</p>
-      <button id = 'buttontwo' onClick={() => otherfunc(x)} style={mystyle}> Press me to clear!</button>
+      <button id = 'buttontwo' style={mystyle}onClick= { () => clear(x)}> Press me to clear!</button>
       <div>
         <Image src={house} alt="Picture of a house"/>
+      <p>{othermsg}</p>
       </div>
     </div>
     </>
   );
+  
 }
