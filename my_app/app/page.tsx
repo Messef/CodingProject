5a3a17c7a70521fd622fceffbeb63cfce9669dc7
Messef <85040583+Messef@users.noreply.github.com>
2023-09-18 16:12:56 -0400

    Added repo to make

--- my_app/app/page.tsx
+++ my_app/app/page.tsx
@@ -5,7 +5,7 @@ import house from './images/house.jpg'
 
 import "./both.js"
 export default function App() {
-  
+
   const windowSize = useRef([window.innerWidth, window.innerHeight]);
   let past_scores = []
   // Backend JS
@@ -16,17 +16,17 @@ export default function App() {
   let timer = 0
   const [count, setCount] = useState(0);
   const countRef = useRef(x);
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-  
-  
+
+
+
   countRef.current = x;
 
   const calculateTimeLeft = () => {
     let year = new Date().getFullYear();
     let difference = +new Date(`10/01/${year}`) - +new Date();
-  
+
     let timeRemaining = {};
-  
+
     if (difference > 0) {
       timeRemaining = {
         days: Math.floor(difference / (1000 * 60 * 60 * 24)),
@@ -35,36 +35,39 @@ export default function App() {
         seconds: Math.floor((difference / 1000) % 60)
       };
     }
-    
+
     return timeRemaining;
   }
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setTimeLeft(calculateTimeLeft());
     }, 1000);
   });
-  function func (x: any ) {
-    
-    if (msg=="Click me to start") {
-      setmsg(msg="Press me to increase the number on screen!")
+  function func(x: any) {
+
+    if (msg == "Click me to start") {
+      setmsg(msg = "Press me to increase the number on screen!")
     }
-    if (x==0) {
-  
+    if (x == 0) {
+
       setTimeout(() => {
-        othersetmsg(othermsg="you got " + countRef.current/time + " cps!")
-        countRef.current=0
-        setx(x=0)
+        othersetmsg(othermsg = "you got " + countRef.current / time + " cps!")
+        countRef.current = 0
+        setx(x = 0)
         alert("You're Done!")
-      
-      
+
+
       }, 5000);
     }
-    
-    setx(x+=1)
+
+    setx(x += 1)
   }
 
   function clear(x: number) {
-      setx(x=0)
+    setx(x = 0)
   }
 
 
@@ -78,27 +81,27 @@ export default function App() {
   };
   return (
     <>
-    <div style={{height: windowSize.current[1], width: windowSize.current[0], background: 'white'}}>
-      
-      <button id = 'button' onClick={() => func(x)} style={mystyle}> {msg}</button>
-      <p></p>
-      <p id='thisthing'>{x}</p>
-      <button id = 'buttontwo' style={mystyle}onClick= { () => clear(x)}> Press me to clear!</button>
-      <p>{timer}</p>
-      <div>
-        <Image src={house} alt="Picture of a house"/>
-      <p>{othermsg}</p>
-      
+      <div style={{ height: windowSize.current[1], width: windowSize.current[0], background: 'white' }}>
 
-<form action="welcome.php" method="get">
-Name: <input type="text" name="name"/><br/>
-E-mail: <input type="text" name="email"/><br/>
-<input type="submit"/>
-</form>
+        <button id='button' onClick={() => func(x)} style={mystyle}> {msg}</button>
+        <p></p>
+        <p id='thisthing'>{x}</p>
+        <button id='buttontwo' style={mystyle} onClick={() => clear(x)}> Press me to clear!</button>
+        <p>{timer}</p>
+        <div>
+          <Image src={house} alt="Picture of a house" />
+          <p>{othermsg}</p>
+          <p></p>
 
+          <form action="welcome.php" method="get">
+            Name: <input type="text" name="name" /><br />
+            E-mail: <input type="text" name="email" /><br />
+            <input type="submit" />
+          </form>
+
+        </div>
       </div>
-    </div>
     </>
   );
-  
+
 }
