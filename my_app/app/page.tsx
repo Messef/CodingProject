fe0db8113e7a39d3345566dcca25f682cccb6f98
Messef <85040583+Messef@users.noreply.github.com>
2023-10-01 10:08:19 -0400

    Cleared everything to create a chess website

--- my_app/app/page.tsx
+++ my_app/app/page.tsx
@@ -1,9 +1,6 @@
 'use client'
 import { useRef, useState, useEffect } from 'react';
-import Image from 'next/image';
-import house from './images/house.jpg'
 
-import "./both.js"
 export default function App() {
 
   
