f64a5c441821f6049055e656ebc882916e4ffd9f
Messef <85040583+Messef@users.noreply.github.com>
2023-11-10 10:11:56 -0500

    did random stuff

--- my_app/app/page.tsx
+++ my_app/app/page.tsx
@@ -1,7 +1,9 @@
 'use client'
+import {motion, AnimatePresence} from "framer-motion";
 import { useRef, useState, useEffect } from 'react';
 import React from 'react';
 import styles from "./my.module.css";
+import router from "next/router";
 console.log(styles.entBack);
 
 export default function App() {
@@ -15,7 +17,8 @@ export default function App() {
   const flex_Container = {
     display: 'flex' as const,
     flexDirection: 'row' as const,
-    justifyContent: "center"
+    justifyContent: "center", 
+    width: "100%"
   }
   const mystyle = {
     color: "black",
@@ -29,7 +32,8 @@ export default function App() {
     paddingBottom: "2%",
     paddingLeft: '7%',
     paddingRight: '7%',
-    backgroundColor: "rgb(255, 255, 255)"
+    backgroundColor: "rgb(255, 255, 255)", 
+    border: "2px solid black"
   }
 
 
@@ -38,17 +42,14 @@ export default function App() {
     
     <>
 
-    <link rel="icon" href="./favicon.ico" />
 
     <body style = {entBack} className  = {styles.entireBack}> 
-      <div style = {flex_Container}>
         <article  style = {childStyle}  >Play Now</article>
         <article  style = {childStyle}   >Puzzles</article>
         <article  style = {childStyle}   >Article</article>
         <article   style = {childStyle}  >Your Stats</article>
         <article   style = {childStyle}  >Study Now</article>
-      </div>
-
+     
       <div>
         <h1 style = {mystyle}>Welcome to freechess.org!</h1>  
       </div>
